8fc9857d9bfe8b68811d1302591e56e64602b116
garciamatias94 <garcia.matias94@gmail.com>
2025-12-05 20:14:44 -0300

    Update test_statistics_utils.py
    A1.2 - test_moving_average_raises_for_invalid_window

--- tests/test_statistics_utils.py
+++ tests/test_statistics_utils.py
@@ -72,7 +72,19 @@ class TestStatisticsUtils(unittest.TestCase):
         - Llamar a moving_average con window=0 (valor no positivo) y verificar que se lanza un ValueError (usar self.assertRaises)
         - Llamar a moving_average con window mayor que la longitud del array y verificar que se lanza un ValueError (usar self.assertRaises)
         """
-
+        numeros = [1, 2, 3]
+        ventana_1 = 0
+        with self.assertRaises(ValueError):
+            StatisticsUtils.moving_average(numeros, ventana_1)
+        
+        ventana_2 = 5  # mayor que la longitud del array
+        with self.assertRaises(ValueError):
+            StatisticsUtils.moving_average(numeros, ventana_2)
+            
+        ventana_3 = -2  # valor negativo
+        with self.assertRaises(ValueError):
+            StatisticsUtils.moving_average(numeros, ventana_3)
+            
     def test_moving_average_only_accepts_1d_sequences(self):
         """Test que verifica que el método moving_average lanza un ValueError cuando
         se llama con una secuencia multidimensional.
